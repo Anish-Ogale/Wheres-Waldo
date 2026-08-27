0da9b28936335891c651903ebe42ba8cf3ebed50
anishogale <anishogale24@gmail.com>
2026-08-27 16:26:40 +0530

    added Leaky ReLU disable mux

--- rtl/Post_Processing/post_process.v
+++ rtl/Post_Processing/post_process.v
@@ -8,11 +8,11 @@ module post_process (
     input [8:0] addr_in,
     input first,
     input last,
+    input relu_enable_r,
     input signed [15:0] scale,
     input [4:0] shift,
     output out_valid,
-    output [63:0] data_out,
-    output [8:0] addr_out
+    output [63:0] data_out
 );
 
     wire acc_valid_out;
@@ -20,30 +20,29 @@ module post_process (
     wire [8:0] acc_addr_out;
     wire [255:0] relu_data_out;
     reg relu_valid_out;
+    reg [255:0] acc_data_out_d;
+    reg relu_enable_d;
+    wire [255:0] post_relu_data;
 
     reg signed [15:0] scale_pipe [0:2];
     reg [4:0] shift_pipe [0:2];
-    reg [8:0] addr_pipe [0:2];
 
     initial begin
         scale_pipe[0] = 0; scale_pipe[1] = 0; scale_pipe[2] = 0;
         shift_pipe[0] = 0; shift_pipe[1] = 0; shift_pipe[2] = 0;
-        addr_pipe[0] = 0; addr_pipe[1] = 0; addr_pipe[2] = 0;
         relu_valid_out = 0;
+        relu_enable_d = 0;
     end
 
     always @(posedge clk) begin
         scale_pipe[0] <= scale;
         shift_pipe[0] <= shift;
-        addr_pipe[0] <= acc_addr_out;
-        
+
         scale_pipe[1] <= scale_pipe[0];
         shift_pipe[1] <= shift_pipe[0];
-        addr_pipe[1] <= addr_pipe[0];
-        
+
         scale_pipe[2] <= scale_pipe[1];
         shift_pipe[2] <= shift_pipe[1];
-        addr_pipe[2] <= addr_pipe[1];
     end
 
     accumulator #(
@@ -71,10 +70,16 @@ module post_process (
     always @(posedge clk) begin
         if (rst) begin
             relu_valid_out <= 1'b0;
+            relu_enable_d <= 1'b0;
+            acc_data_out_d <= 256'd0;
         end else begin
             relu_valid_out <= acc_valid_out;
+            relu_enable_d <= relu_enable_r;
+            acc_data_out_d <= acc_data_out;
         end
     end
+
+    assign post_relu_data = relu_enable_d ? relu_data_out : acc_data_out_d;
 
     quantizer #(
         .NUM_CHANNELS(8)
@@ -85,10 +90,8 @@ module post_process (
         .out_valid(out_valid),
         .scale(scale_pipe[2]),
         .shift(shift_pipe[2]),
-        .in_data_flat(relu_data_out),
+        .in_data_flat(post_relu_data),
         .out_data_flat(data_out)
     );
-
-    assign addr_out = addr_pipe[2];
 
 endmodule

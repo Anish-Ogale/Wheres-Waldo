d845479a72ba2a556bed6c34af4c04f047024fa0
anishogale <anishogale24@gmail.com>
2026-08-28 16:19:46 +0530

    added top.v and fixed sys array

--- rtl/calculation_block/systolic_array/sys_para.v
+++ rtl/calculation_block/systolic_array/sys_para.v
@@ -22,6 +22,24 @@ module sys_para #(
         for (i = 0; i < size; i = i + 1) begin : row
             for (j = 0; j < size; j = j + 1) begin : column
 
+                wire signed [7:0] p_in;
+                wire signed [31:0] s_in;
+                wire signed [7:0] w_in;
+
+                if (j == 0) begin : gen_p0
+                    assign p_in = pixel[(i*8) +: 8];
+                end else begin : gen_pn
+                    assign p_in = pixel_temp[i][j-1];
+                end
+
+                if (i == 0) begin : gen_sw0
+                    assign s_in = 32'sd0;
+                    assign w_in = weight[(j*8) +: 8];
+                end else begin : gen_swn
+                    assign s_in = sum_temp[i-1][j];
+                    assign w_in = weight_temp[i-1][j];
+                end
+
                 MAC #(
                     .pixel_width(8),
                     .sum_width(32),
@@ -29,21 +47,9 @@ module sys_para #(
                 ) M (
                     .clk(clk),
                     .rst(rst),
-                    .pixel_in(
-                        (j == 0) ?
-                        pixel[(i*8) +: 8] :
-                        pixel_temp[i][j-1]
-                    ),
-                    .sum_in(
-                        (i == 0) ?
-                        32'sd0 :
-                        sum_temp[i-1][j]
-                    ),
-                    .weight(
-                        (i == 0) ?
-                        weight[(j*8) +: 8] :
-                        weight_temp[i-1][j]
-                    ),
+                    .pixel_in(p_in),
+                    .sum_in(s_in),
+                    .weight(w_in),
                     .sum_out(sum_temp[i][j]),
                     .pixel_out(pixel_temp[i][j]),
                     .load_en(load_en),
